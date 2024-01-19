e40dc4bb358dc18ef13be2155d6d4d765b169ddc
Felipe Rezende <felipe.rezende@avenuecode.com>
2024-01-18 21:51:50 -0300

    feat: add axios and create list products

--- src/components/ListProducts.vue
+++ src/components/ListProducts.vue
@@ -1,10 +1,33 @@
 <template>
-  <p>lista</p>
+  <section class="produtos-container">
+    <div v-for="product in products" :key="product.id">
+      <img v-if="product.fotos" :src="product.fotos[0].src" :alt="product.fotos[0].titulo">
+      <p class="preco">{{ product.preco }}</p>
+      <h2 class="titulo">{{ product.nome }}</h2>
+      <p>{{ product.descricao }}</p>
+    </div>
+  </section>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name: "ListProducts"
+    name: "ListProducts",
+    data() {
+      return {
+        products: null
+      }
+    },
+    created() {
+      this.getProducts()
+    },
+    methods: {
+      getProducts() {
+        axios.get('https://raw.githubusercontent.com/rezendefelipe/vue-ranek/main/ranek-api/ranek.json')
+          .then(data => this.products = data.data.produto)
+      }
+    }
 }
 </script>
 
