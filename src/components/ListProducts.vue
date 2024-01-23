20fffaad403d6508b05bfa87b844c8af5457b443
Felipe Rezende <felipe.rezende@avenuecode.com>
2024-01-23 19:59:34 -0300

    feat: add style to list products and no result

--- src/components/ListProducts.vue
+++ src/components/ListProducts.vue
@@ -1,10 +1,19 @@
 <template>
-  <section class="produtos-container">
-    <div v-for="product in products" :key="product.id">
-      <img v-if="product.fotos" :src="product.fotos[0].src" :alt="product.fotos[0].titulo">
-      <p class="preco">{{ product.preco }}</p>
-      <h2 class="titulo">{{ product.nome }}</h2>
-      <p>{{ product.descricao }}</p>
+  <section class="products-container">
+    <div v-if="products.length > 0" class="products">
+      <div v-for="product in products" :key="product.id" class="product">
+        <router-link to="/">
+          <img v-if="product.fotos" :src="product.fotos[0].src" :alt="product.fotos[0].titulo">
+          <p class="price">{{ product.preco }}</p>
+          <h2 class="title">{{ product.nome }}</h2>
+          <p>{{ product.descricao }}</p>
+        </router-link>
+      </div>
+    </div>
+    <div v-else-if="products.length === 0" class="no-result">
+      <p>
+        Busca sem resultados
+      </p>
     </div>
   </section>
 </template>
@@ -17,7 +26,7 @@ export default {
     name: "ListProducts",
     data() {
       return {
-        products: null,
+        products: [],
         pagination: 9
       }
     },
@@ -44,6 +53,49 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+  .products-container {
+    max-width: 1000px;
+    margin: 0 auto;
+  }
 
+  .products {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 30px;
+    margin: 30px;
+  }
+
+  .product {
+    box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+    padding: 10px;
+    background: #fff;
+    border-radius: 4px;
+    transition: all 0.2s;
+  }
+
+  .product:hover {
+    box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+    transform: scale(1.1);
+    position: relative;
+    z-index: 1;
+  }
+
+  .product img {
+    border-radius: 4px;
+    margin-bottom: 20px;
+  }
+
+  .title {
+    margin-bottom: 10px;
+  }
+
+  .price {
+    color: #e80;
+    font-weight: bold;
+  }
+
+  .no-result {
+    text-align: center;
+  }
 </style>
