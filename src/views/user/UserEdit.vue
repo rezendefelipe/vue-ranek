24da9a36fb479352128c7140758f3d3b54ef4e9f
Felipe Rezende <felipe.rezende@avenuecode.com>
2024-02-01 17:44:11 -0300

    feat: add update user

--- src/views/user/UserEdit.vue
+++ src/views/user/UserEdit.vue
@@ -1,10 +1,30 @@
 <template>
-  <p>UserEdit</p>
+  <section>
+    <UserForm>
+      <button class="btn btn-form" @click.prevent="updateUser">Atualizar Usuário</button>
+    </UserForm>
+  </section>
 </template>
 
 <script>
+import UserForm from "@/components/UserForm.vue";
+import { api } from "@/services.js";
+
 export default {
-    name: "UserEdit"
+  name: "UserEdit",
+  components: {
+    UserForm
+  },
+  methods: {
+    updateUser() {
+      api.put(`/usuario/${ this.$store.state.user.id }`, this.$store.state.user).then(() => {
+        this.$store.dispatch("getUser", this.$store.state.user.id);
+        this.$router.push({ name: "user" })
+      }).catch(err => {
+        console.log(err);
+      })
+    }
+  }
 }
 </script>
 
