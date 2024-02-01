317353313dd310a992c4dad1dc841dac6f5ea418
Felipe Rezende <felipe.rezende@avenuecode.com>
2024-02-01 17:33:59 -0300

    feat: add remove product

--- src/views/user/UserProducts.vue
+++ src/views/user/UserProducts.vue
@@ -10,6 +10,9 @@
         <li v-for="product in userProducts" :key="product.email" class="item-li">
           <ProductItens :product="product" class="item">
             <p>{{ product.descricao }}</p>
+            <button class="delete" @click="deleteProduct(product.id)">
+              Deletar
+            </button>
           </ProductItens>
         </li>
       </ul>
@@ -22,6 +25,7 @@ import AddProducts from "@/components/AddProducts.vue";
 import ProductItens from "@/components/ProductItens.vue";
 
 import { mapState, mapActions } from "vuex";
+import { api } from "@/services.js";
 
 export default {
     name: "UserProducts",
@@ -33,7 +37,15 @@ export default {
       ...mapState(['login', 'user', 'userProducts']),
     },
     methods: {
-      ...mapActions(['getUserProducts'])
+      ...mapActions(['getUserProducts']),
+      deleteProduct(id) {
+        const confirm = window.confirm("Deseja remover esse produto?");
+        if (confirm) api.delete(`/produto/${ id }`).then(() => {
+          this.getUserProducts();
+        }).catch(err => {
+          console.log(err);
+        })
+      }
     },
     watch: {
       login() {
@@ -62,5 +74,18 @@ export default {
   .list-enter-active,
   .list-leave-active {
     transition: all .3s;
+  }
+
+  .delete {
+    position: absolute;
+    right: 0px;
+    top: 0px;
+    background: url("../../assets/remove.svg") no-repeat center center;
+    width: 24px;
+    height: 24px;
+    text-indent: -140px;
+    overflow: hidden;
+    cursor: pointer;
+    border: none;
   }
 </style>
