b0ad3b8c8f54a37d55cb75f0f38d2717ad8a7154
Felipe Rezende <felipe.rezende@avenuecode.com>
2024-02-01 20:13:57 -0300

    feat: add buys and sells page

--- src/views/user/UserBuys.vue
+++ src/views/user/UserBuys.vue
@@ -1,13 +1,66 @@
 <template>
-  <p>UserBuys</p>
+  <section>
+    <div v-if="buys">
+      <h2>Compras</h2>
+      <div class="products-wrap" v-for="(buy, index) in buys" :key="index">
+        <ProductItens v-if="buy.produto" :product="buy.produto">
+          <p class="seller">
+            <span>Vendedor: </span> {{ buy.vendedor_id }}
+          </p>
+        </ProductItens>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import ProductItens from '@/components/ProductItens.vue';
+import { api } from "@/services.js";
+import { mapState } from "vuex";
+
 export default {
-    name: "UserBuys"
+    name: "UserBuys",
+    components: {
+      ProductItens
+    },
+    computed: {
+        ...mapState(["user", "login"]),
+    },
+    data() {
+      return {
+        buys: null
+      }
+    },
+    methods: {
+      getBuys() {
+        api.get(`/transacao?comprador_id=${ this.user.id }`).then(resp => {
+          this.buys = resp.data
+        });
+      }
+    },
+    watch: {
+      login() {
+        this.getBuys();
+      }
+    },
+    created() {
+      if (this.login) {
+        this.getBuys();
+      }
+    }
 }
 </script>
 
-<style>
+<style scoped>
+  .products-wrap {
+    margin-bottom: 40px;
+  }
 
+  .seller span {
+    color: #e80;
+  }
+
+  h2 {
+    margin-bottom: 20px;
+  }
 </style>
--- src/views/user/UserSells.vue
+++ src/views/user/UserSells.vue
@@ -1,13 +1,84 @@
 <template>
-  <p>UserSells</p>
+  <section>
+    <div v-if="sells">
+      <h2>Vendas</h2>
+      <div class="products-wrap" v-for="(sell, index) in sells" :key="index">
+        <ProductItens v-if="sell.produto" :product="sell.produto">
+          <p class="seller">
+            <span>Comprador: </span> {{ sell.comprador_id }}
+          </p>
+        </ProductItens>
+        <div class="address">
+          <h3>Entrega: </h3>
+          <ul v-if="sell.endereco">
+            <li v-for="(value, index) in sell.endereco" :key="index">{{ `${index}: ${value}` }}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import ProductItens from '@/components/ProductItens.vue';
+import { api } from "@/services.js";
+import { mapState } from "vuex";
+
 export default {
-    name: "UserSells"
+    name: "UserSells",
+    components: {
+      ProductItens
+    },
+    computed: {
+        ...mapState(["user", "login"]),
+    },
+    data() {
+      return {
+        sells: null
+      }
+    },
+    methods: {
+      getSells() {
+        api.get(`/transacao?vendedor_id=${ this.user.id }`).then(resp => {
+          this.sells = resp.data
+        });
+      }
+    },
+    watch: {
+      login() {
+        this.getSells();
+      }
+    },
+    created() {
+      if (this.login) {
+        this.getSells();
+      }
+    }
 }
 </script>
 
-<style>
+<style scoped>
+  .products-wrap {
+    margin-bottom: 40px;
+  }
 
+  .seller span {
+    color: #e80;
+  }
+
+  h2 {
+    margin-bottom: 20px;
+  }
+
+  .address {
+    display: grid;
+    grid-template-columns: minmax(100px, 200px) 1fr;
+    grid-gap: 20px;
+    margin-bottom: 60px;
+  }
+
+  h3 {
+    margin: 0;
+    justify-self: end;
+  }
 </style>
